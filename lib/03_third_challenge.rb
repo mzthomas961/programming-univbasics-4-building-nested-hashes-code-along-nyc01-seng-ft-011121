7ce51c8c132a8a8587474960fa6120d8af4c9e1f
Matthew Thomas <mzthomas96@gmail.com>
2021-01-02 19:25:39 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,16 +1,10 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-       "Lord Montague", age: "53"
-        },
-      :matriarch => {
-       name: "Lady Montague", age: "54"
-        },
-      :hero => {
-       name: "Romeo", age: "15", status: "alive"
-        },
-      :hero_friends => []
+    :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
